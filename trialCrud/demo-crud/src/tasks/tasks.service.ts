1ac5af9b82b3898e1c3c394724ad16247cbec8ea
parthpsp1 <pspparth1998@gmail.com>
2022-11-09 13:00:48 +0530

    Created GET Service, Controller and Model for task for task CRUD

--- trialCrud/demo-crud/src/tasks/tasks.service.ts
+++ trialCrud/demo-crud/src/tasks/tasks.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
 
 @Injectable()
 export class TasksService {
-    private tasks = [];
+    private tasks: Task[] = [];
 
-    getTasks() {
+    getTasks(): Task[] {
         return this.tasks;
     }
 }
